68841ec3a11b5496fd5ae20bc4714a655ee55df7
sjbeskur <sam.beskur@gmail.com>
2019-08-21 21:02:08 -0600

    import

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate rand;
+
+const PASSCODE_LEN: usize = 8;
+
+fn main() {
+
+    for i in (0..100) {
+        let mut password = gen_string(PASSCODE_LEN);
+        password.insert(PASSCODE_LEN/2, '-');
+        println!("{:?}", password);
+    }
+}
+
+fn gen_string(len: usize) -> String{
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ACDEFGHIJKLMNPQRSTUVWXYZ\
+                            23456789";
+    let mut rng = rand::thread_rng();
+    let password: String = (0..len)
+        .map(|_| {
+            let i = rng.gen_range(0, CHARSET.len());
+            char::from(unsafe { *CHARSET.get_unchecked(i) }) //(i is in the range of CHARSET)
+            //char::from({ *CHARSET.get(i).unwrap() }) // safe but slower 
+        }).collect();
+    password
+
+}
